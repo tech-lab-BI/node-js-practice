414aeba7ac187f5b1bede4e6026f5613ba31c65b
bikram <techmarketu9@gmail.com>
2026-07-16 08:36:11 +0530

    dynamic UI complete

--- 9-dynamic-UI/app.js
+++ 9-dynamic-UI/app.js
@@ -22,7 +22,8 @@ app.use("/user", userRouter);
 app.use("/admin", adminRouter);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "pageNotFound.html"));
+  // res.status(404).sendFile(path.join(__dirname, "views", "pageNotFound.html"));
+  res.status(404).render("pageNotFound", {pageTitle: "Page Not Found"});
 });
 
 app.listen(3001, () => {
--- 9-dynamic-UI/routes/user.js
+++ 9-dynamic-UI/routes/user.js
@@ -10,22 +10,24 @@ userRouter.use(express.urlencoded());
 
 userRouter.get("/home", (req, res, next) => {
   // res.sendFile(path.join(__dirname, "../", "views", "home.html"));
-  res.render("home", { homes });
+  res.render("home", { homes:homes, pageTitle: 'Home Page' });
 });
 
 userRouter.get("/contact-us", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "contactUs.html"));
+  // res.sendFile(path.join(__dirname, "../", "views", "contactUs.html"));
+  res.render("contactUs", {pageTitle: "Contact Page"});
 });
 
 userRouter.get("/register-home", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "registerHome.html"));
+  // res.sendFile(path.join(__dirname, "../", "views", "registerHome.html"));
+  res.render("registerHome", {pageTitle: "Home Registration"});
 });
 
 userRouter.post("/register-home", (req, res, next) => {
   res.send(`<h1>Home registration successfully.</h1><br>
         <button><a href="/user/home">Check Home List</a></button>`);
   console.log("Home : ", req.body.name);
-  homes.push({ houseName: req.body.name });
+  homes.push({ houseName: req.body.name, price: req.body.price, location:req.body.location, rating:req.body.rating, photoUrl:req.body.photoUrl });
 });
 
 // module.exports = {
