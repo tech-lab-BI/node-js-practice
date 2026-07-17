7d5ad6c32b3c4591a571d22be521d86440df95d3
bikram <techmarketu9@gmail.com>
2026-07-17 12:07:29 +0530

    mvc adv , nodemon fix

--- 10-mvc/controllers/userPage.js
+++ 10-mvc/controllers/userPage.js
@@ -1,22 +1,24 @@
-const Home = require('../models/home');
+const Home = require("../models/home");
 
 exports.getContact = (req, res, next) => {
-  res.render("contactUs", {pageTitle: "Contact Page"});
+  res.render("contactUs", { pageTitle: "Contact Page" });
 };
 
 exports.getRegisterHome = (req, res, next) => {
-  res.render("registerHome", {pageTitle: "Home Registration"});
+  res.render("registerHome", { pageTitle: "Home Registration" });
 };
 
 exports.postRegisterHome = (req, res, next) => {
   res.send(`<h1>Home registration successfully.</h1><br>
         <button><a href="/user/home">Check Home List</a></button>`);
-  const {houseName , price, location, rating, photoUrl} = req.body;
-  const home = new Home(houseName , price, location, rating, photoUrl);
+  const { houseName, price, location, rating, photoUrl } = req.body;
+  const home = new Home(houseName, price, location, rating, photoUrl);
   home.save();
 };
 
 exports.getHome = (req, res, next) => {
-  const homes = Home.fetchAll();
-  res.render("home", { homes:homes, pageTitle: 'Home Page' });
+  const homes = Home.fetchAll((homes) => {
+    res.render("home", { homes: homes, pageTitle: "Home Page" });
+  });
+  // res.render("home", { homes: homes, pageTitle: "Home Page" });
 };
--- 10-mvc/models/home.js
+++ 10-mvc/models/home.js
@@ -1,17 +1,34 @@
-const homes = [];
+//core modules
+const fs = require("fs");
+const path = require("path");
 
-module.exports = class Home{
-    constructor(houseName, price, location, rating, photoUrl){
-        this.houseName = houseName;
-        this.price = price;
-        this.location = location;
-        this.rating = rating;
-        this.photoUrl = photoUrl;
-    }
-    save(){
-        homes.push(this);
-    }
-    static fetchAll(){
-        return homes;
-    }
-}
+// const homes = [];
+
+module.exports = class Home {
+  constructor(houseName, price, location, rating, photoUrl) {
+    this.houseName = houseName;
+    this.price = price;
+    this.location = location;
+    this.rating = rating;
+    this.photoUrl = photoUrl;
+  }
+  save() {
+    Home.fetchAll((homeList) => {
+      homeList.push(this); //(err) data folder change so nodemon restart homes get empty - sol>>nodemon.json
+      const filePath = path.join(__dirname, "../", "data", "homes.json");
+      fs.writeFile(filePath, JSON.stringify(homeList), (err) => {
+        console.log(err);
+      });
+    });
+  }
+  static fetchAll(callback) {
+    const filePath = path.join(__dirname, "../", "data", "homes.json");
+    fs.readFile(filePath, (err, data) => {//read/wrote file async function
+      //   if (err) {
+      //     callback([]);
+      //   }
+      //   callback(JSON.parse(data));
+      callback(err ? [] : JSON.parse(data));
+    });
+  }
+};
