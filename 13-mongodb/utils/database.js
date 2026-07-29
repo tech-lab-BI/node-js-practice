a1f78e30b3977435272683ec93b490e2be54170e
BIKRAM SAMANTA <162464528+tech-lab-BI@users.noreply.github.com>
2026-07-29 10:55:55 +0530

    Modify MongoDB
    Updated MongoDB connection URL to use placeholders for admin name and password.

--- 13-mongodb/utils/database.js
+++ 13-mongodb/utils/database.js
@@ -2,7 +2,7 @@ const mongodb = require('mongodb');
 
 const MongoClient = mongodb.MongoClient;
 
-const url = "mongodb://root:root@ac-hb1tzqd-shard-00-00.fhpqazo.mongodb.net:27017,ac-hb1tzqd-shard-00-01.fhpqazo.mongodb.net:27017,ac-hb1tzqd-shard-00-02.fhpqazo.mongodb.net:27017/?ssl=true&replicaSet=atlas-km16gb-shard-0&authSource=admin&appName=test";
+const url = "mongodb://<admin_name>:<password>@ac-hb1tzqd-shard-00-00.fhpqazo.mongodb.net:27017,ac-hb1tzqd-shard-00-01.fhpqazo.mongodb.net:27017,ac-hb1tzqd-shard-00-02.fhpqazo.mongodb.net:27017/?ssl=true&replicaSet=atlas-km16gb-shard-0&authSource=admin&appName=test";
 
 let _db;
 
