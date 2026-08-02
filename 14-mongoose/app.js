2f588cda336ad16602f2e44d20132922a38ecd42
bikram <techmarketu9@gmail.com>
2026-08-02 08:43:10 +0530

    mongoose connection ok

---
+++ 14-mongoose/app.js
@@ -0,0 +1,36 @@
+//core module
+const path = require('path');
+//external module
+const express = require('express');
+const { default: mongoose } = require('mongoose');
+//local module
+const userRouter = require('./routes/user');
+const adminRouter = require('./routes/admin');
+
+const app = express();
+
+
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, "views", "loginPage.html"));
+})
+
+app.use('/user', userRouter);
+app.use('/admin', adminRouter);
+
+app.use((req, res, next) => {
+    res.status(404).render('404page', { pageName: "Error Page"});
+})
+
+const dbPath = "mongodb://<admin>:<password>@ac-hb1tzqd-shard-00-00.fhpqazo.mongodb.net:27017,ac-hb1tzqd-shard-00-01.fhpqazo.mongodb.net:27017,ac-hb1tzqd-shard-00-02.fhpqazo.mongodb.net:27017/airbnb?ssl=true&replicaSet=atlas-km16gb-shard-0&authSource=admin&appName=test";
+const PORT = 3001;
+
+mongoose.connect(dbPath).then(() => {
+    app.listen(PORT, () => {console.log("SERVER START......")});
+});
---
+++ 14-mongoose/controllers/adminController.js
@@ -0,0 +1,90 @@
+//local module
+const {
+  putData,
+  getData,
+  findById,
+  deleteById,
+} = require("../model/homesFunc");
+const { deleteByIdFav } = require("../model/favouriteFunc");
+
+exports.adminPage = (req, res, next) => {
+  getData().then((homes) => {
+    res.render("admin/adminHome", { pageName: "Admin Panel", homes: homes });
+  });
+};
+
+exports.hostHomePage = (req, res, next) => {
+  getData().then((homes) => {
+    res.render("admin/hostHomes", { pageName: "Admin Panel", homes: homes });
+  });
+};
+
+exports.postHostHomePage = (req, res, next) => {
+  console.log(req.body);
+  const home = {
+    houseName: req.body.houseName,
+    price: Number(req.body.price),
+    location: req.body.location,
+    rating: parseFloat(req.body.rating),
+    photoUrl: req.body.photoUrl,
+    description: req.body.description,
+  };
+
+  putData(req.body._id, home)
+    .then(() => {
+      res.redirect("/admin/hostHome");
+    })
+    .catch(console.log);
+};
+
+exports.registrationPage = (req, res, next) => {
+  res.render("admin/editHomePage", {
+    pageName: "Home Registration",
+    editing: false,
+  });
+};
+
+exports.editHomePage = (req, res, next) => {
+  const _id = req.params.homeId;
+  const editing = req.query.editing; // return string
+
+  findById(_id).then((home) => {
+    res.render("admin/editHomePage", {
+      pageName: "Edit Home",
+      home: home,
+      editing: editing,
+    });
+  });
+};
+
+exports.postEditHomePage = (req, res, next) => {
+  const home = {
+    houseName: req.body.houseName,
+    price: Number(req.body.price),
+    location: req.body.location,
+    rating: Number(req.body.rating),
+    photoUrl: req.body.photoUrl,
+    description: req.body.description,
+  };
+  
+  putData(req.body.id, home)
+    .then(() => {
+      res.redirect("/admin/hostHome");
+    })
+    .catch(console.log);
+};
+
+exports.contactSubmitPage = (req, res, next) => {
+  res.send(`
+    <h1>Thank You for Contact We will reach you shortly........</h1>
+    <a href="/">Back to Login</a>
+  `);
+};
+
+exports.deleteHome = (req, res, next) => {
+  deleteById(req.params.homeId).then(() => {
+    // deleteByIdFav(req.params.homeId, () => {
+    res.redirect("/admin/hostHome");
+    // });
+  });
+};
